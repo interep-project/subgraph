d78c01236431ac6478ba22309dd2ce5fd7ba1a23
cedoor <me@cedoor.dev>
2022-02-23 17:01:59 +0100

    refactor: update interep contract

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,112 +1,78 @@
-import { BigInt, log, ByteArray } from "@graphprotocol/graph-ts"
-import {
-    GroupAdded,
-    IdentityCommitmentAdded,
-    IdentityCommitmentDeleted,
-    OffchainRoot
-} from "../generated/Groups/Groups"
-import { Group, Member, OffchainGroup } from "../generated/schema"
+import { BigInt, log, ByteArray, crypto } from "@graphprotocol/graph-ts"
+import { GroupAdded, MemberAdded, MemberRemoved, OffchainGroupAdded } from "../generated/Interep/Interep"
+import { OnchainGroup, Member, OffchainGroup } from "../generated/schema"
 import { concat, hash } from "./utils"
 
 /**
  * Adds an offchain group in the storage.
- * @param event Ethereum event emitted when an offchain root is published.
+ * @param event Ethereum event emitted when an offchain group is published.
  */
-export function updateOffchainGroup(event: OffchainRoot): void {
-    log.debug(`OffchainRoot event block: {}`, [event.block.number.toString()])
+export function updateOffchainGroup(event: OffchainGroupAdded): void {
+    log.debug(`OffchainGroupAdded event block: {}`, [event.block.number.toString()])
 
-    const groupId = hash(concat(event.params.provider, event.params.name))
-    let group = OffchainGroup.load(groupId)
+    let group = OffchainGroup.load(event.params.groupId.toString())
 
     // Creates the group if it is not exist.
     if (group === null) {
-        log.info("Creating offchain group '{}' with provider '{}' and name '{}'", [
-            groupId,
-            event.params.provider.toString(),
-            event.params.name.toString()
-        ])
+        log.info("Creating offchain group '{}'", [event.params.groupId.toString()])
 
-        group = new OffchainGroup(groupId)
+        group = new OffchainGroup(event.params.groupId.toString())
 
-        group.provider = event.params.provider.toString()
-        group.name = event.params.name.toString()
-        group.roots = [event.params.root]
+        group.root = event.params.root
+        group.depth = event.params.depth
 
         group.save()
 
-        log.info("Offchain group with provider '{}' and name '{}' has been created", [
-            group.id,
-            event.params.provider.toString(),
-            event.params.name.toString()
-        ])
+        log.info("Offchain group '{}' has been created", [group.id])
     }
-    // Add the root to an existing offchain group.
+    // Update the root and the depth of an existing offchain group.
     else {
-        group.roots.push(event.params.root)
+        group.root = event.params.root
+        group.depth = event.params.depth
 
         group.save()
 
-        log.info("Merkle root of the offchain group with provider '{}' and name '{}' has been added", [
-            group.id,
-            event.params.provider.toString(),
-            event.params.name.toString()
-        ])
+        log.info("Offchain group '{}' has been updated", [group.id])
     }
 }
 
 /**
  * Adds a group in the storage.
- * @param event Ethereum event emitted when a group is created.
+ * @param event Ethereum event emitted when a onchain group is created.
  */
-export function addGroup(event: GroupAdded): void {
+export function createOnchainGroup(event: GroupAdded): void {
     log.debug(`GroupAdded event block: {}`, [event.block.number.toString()])
 
-    const groupId = hash(concat(event.params.provider, event.params.name))
-    const group = new Group(groupId)
+    const group = new OnchainGroup(event.params.groupId.toString())
 
-    log.info("Creating group '{}' with provider '{}' and name '{}'", [
-        group.id,
-        event.params.provider.toString(),
-        event.params.name.toString()
-    ])
+    log.info("Creating onchain group '{}'", [group.id])
 
-    group.provider = event.params.provider.toString()
-    group.name = event.params.name.toString()
     group.depth = event.params.depth
     group.size = 0
 
     group.save()
 
-    log.info("Group with provider '{}' and name '{}' has been created", [
-        group.id,
-        event.params.provider.toString(),
-        event.params.name.toString()
-    ])
+    log.info("Onchain group '{}' has been created", [group.id])
 }
 
 /**
  * Adds a member in a group.
- * @param event Ethereum event emitted when an identity commitment is added to a group.
+ * @param event Ethereum event emitted when a member is added to a group.
  */
-export function addMember(event: IdentityCommitmentAdded): void {
-    log.debug(`IdentityCommitmentAdded event block {}`, [event.block.number.toString()])
+export function addMember(event: MemberAdded): void {
+    log.debug(`MemberAdded event block {}`, [event.block.number.toString()])
 
-    const groupId = hash(concat(event.params.provider, event.params.name))
-    const group = Group.load(groupId)
+    const group = OnchainGroup.load(event.params.groupId.toString())
 
     if (group) {
         const memberId = hash(
-            concat(ByteArray.fromBigInt(event.params.identityCommitment), ByteArray.fromHexString(groupId))
+            concat(ByteArray.fromBigInt(event.params.identityCommitment), ByteArray.fromBigInt(event.params.groupId))
         )
         const member = new Member(memberId)
 
-        log.info("Adding member '{}' in the group with provider '{}' and name '{}'", [
-            member.id,
-            event.params.provider.toString(),
-            event.params.name.toString()
-        ])
+        log.info("Adding member '{}' in the onchain group '{}'", [member.id, group.id])
 
-        member.group = groupId
+        member.group = group.id
         member.identityCommitment = event.params.identityCommitment
         member.root = event.params.root
         member.index = group.size
@@ -117,41 +83,28 @@ export function addMember(event: IdentityCommitmentAdded): void {
 
         group.save()
 
-        log.info("Member '{}' of the group with provider '{}' and name '{}' has been added", [
-            member.id,
-            event.params.provider.toString(),
-            event.params.name.toString()
-        ])
+        log.info("Member '{}' of the onchain group '{}' has been added", [member.id, group.id])
     }
 }
 
 /**
- * Deletes an identity commitment from a group.
- * @param event Ethereum event emitted when an identity commitment is deleted from a group.
+ * Removes a member from a group.
+ * @param event Ethereum event emitted when a member is removed from a group.
  */
-export function deleteMember(event: IdentityCommitmentDeleted): void {
-    log.debug(`IdentityCommitmentDeleted event block {}`, [event.block.number.toString()])
+export function removeMember(event: MemberRemoved): void {
+    log.debug(`MemberRemoved event block {}`, [event.block.number.toString()])
 
-    const groupId = hash(concat(event.params.provider, event.params.name))
     const memberId = hash(
-        concat(ByteArray.fromBigInt(event.params.identityCommitment), ByteArray.fromHexString(groupId))
+        concat(ByteArray.fromBigInt(event.params.identityCommitment), ByteArray.fromBigInt(event.params.groupId))
     )
     const member = new Member(memberId)
 
-    log.info("Deleting member '{}' from the group with provider '{}' and name '{}'", [
-        member.id,
-        event.params.provider.toString(),
-        event.params.name.toString()
-    ])
+    log.info("Removing member '{}' from the onchain group '{}'", [member.id, event.params.groupId.toString()])
 
-    member.identityCommitment = BigInt.zero()
+    member.identityCommitment = BigInt.fromByteArray(crypto.keccak256(ByteArray.fromUTF8("Semaphore")))
     member.root = event.params.root
 
     member.save()
 
-    log.info("Member '{}' of the group with provider '{}' and name '{}' has been deleted", [
-        member.id,
-        event.params.provider.toString(),
-        event.params.name.toString()
-    ])
+    log.info("Member '{}' of the onchain group '{}' has been removed", [member.id, event.params.groupId.toString()])
 }
