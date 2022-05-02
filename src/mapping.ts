2283d433ced4fe685265f915a3125128cbb0a7c3
cedoor <me@cedoor.dev>
2022-05-02 15:47:06 +0200

    refactor: replace groups with merkle trees

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,40 +1,25 @@
 import { log } from "@graphprotocol/graph-ts"
-import { GroupUpdated } from "../generated/Interep/Interep"
-import { Group } from "../generated/schema"
+import { MerkleTreeAdded } from "../generated/Interep/Interep"
+import { MerkleTree } from "../generated/schema"
 
 /**
- * Updates an Interep group.
- * @param event Ethereum event emitted when new Interep groups are saved on-chain.
+ * Adds an Interep Merkle tree.
+ * @param event Ethereum event emitted when new Interep Merkle trees are saved on-chain.
  */
 // eslint-disable-next-line import/prefer-default-export
-export function updateGroup(event: GroupUpdated): void {
-    log.debug(`GroupUpdated event block: {}`, [event.block.number.toString()])
+export function addMerkleTree(event: MerkleTreeAdded): void {
+    log.debug(`MerkleTreeAdded event block: {}`, [event.block.number.toString()])
 
-    let group = Group.load(event.params.groupId.toString())
+    let group = MerkleTree.load(event.params.root.toString())
 
-    // Creates the group if it is not exist.
-    if (group === null) {
-        log.info("Creating Interep group '{}'", [event.params.groupId.toString()])
+    log.info("Creating Interep Merkle tree '{}'", [event.params.root.toString()])
 
-        group = new Group(event.params.groupId.toString())
+    group = new MerkleTree(event.params.root.toString())
 
-        group.provider = event.params.provider.toString()
-        group.name = event.params.name.toString()
-        group.root = event.params.root
-        group.depth = event.params.depth
+    group.root = event.params.root
+    group.depth = event.params.depth
 
-        group.save()
+    group.save()
 
-        log.info("Interep group '{}' has been created", [group.id])
-    }
-
-    // Update the root and the depth of an existing Interep group.
-    else {
-        group.root = event.params.root
-        group.depth = event.params.depth
-
-        group.save()
-
-        log.info("Interep group '{}' has been updated", [group.id])
-    }
+    log.info("Interep group '{}' has been created", [group.id])
 }
